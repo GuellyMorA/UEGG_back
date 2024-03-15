2982dda3b3daff38b288c0ed95d2ae72e41239ae
Guelly Morales <guesismoa@gmail.com>
2024-03-15 00:18:05 -0400

    modificacion ultima

--- controllers/uegg/ueggEmbInformeEmbarazo.js
+++ controllers/uegg/ueggEmbInformeEmbarazo.js
@@ -1,27 +1,94 @@
-const UeggPcpaConstruccion = require('../../models/uegg').uegg_pcpa_construccion ; 
+const UeggEmbInformeEmbarazo = require('../../models/uegg').uegg_emb_informe_embarazo ; 
 
 module.exports = {                                                                                                                                                                                                                                                                                                                                                                                                                             
     list(req, res) {
-        return UeggPcpaConstruccion
+        return UeggEmbInformeEmbarazo
             .findAll({})
-            .then((ueggPcpaConstruccion) => res.status(200).send(ueggPcpaConstruccion)) 
+            .then((ueggEmbInformeEmbarazo) => res.status(200).send(ueggEmbInformeEmbarazo)) 
             .catch((error) => { res.status(400).send(error); });
     },
 
     getById(req, res) {
         console.log(req.params.id); 
-        return UeggPcpaConstruccion
+        return UeggEmbInformeEmbarazo
             .findByPk(req.params.id)
-            .then((ueggPcpaConstruccion) => { 
-                console.log(ueggPcpaConstruccion);
-                if (!ueggPcpaConstruccion) {
+            .then((ueggEmbInformeEmbarazo) => { 
+                console.log(ueggEmbInformeEmbarazo);
+                if (!ueggEmbInformeEmbarazo) {
                     return res.status(404).send({
-                        message: 'UeggPcpaConstruccion no encontrado',
+                        message: 'UeggEmbInformeEmbarazo no encontrado',
                     });
                 }
-                return res.status(200).send(ueggPcpaConstruccion); 
+                return res.status(200).send(ueggEmbInformeEmbarazo); 
             })
             .catch((error) => res.status(400).send(error));
     },
+
+    add(req, res) {
+        return UeggEmbInformeEmbarazo.create({
+            id_pcpa_unidad_eductiva: req.body.id_pcpa_unidad_eductiva,
+            id_emb_reporte_embarazo_tipo_1: req.body.id_emb_reporte_embarazo_tipo_1, 
+            id_emb_reporte_embarazo_tipo_2: req.body.id_emb_reporte_embarazo_tipo_2, 
+            id_emb_reporte_embarazo_tipo_3: req.body.id_emb_reporte_embarazo_tipo_3, 
+    
+            estado: 'ACTIVO' ,
+            usu_cre: req.body.usu_cre ,
+            fec_cre: req.body.fec_cre 
+          
+        })
+          .then(ueggEmbInformeEmbarazo => res.status(201).send(ueggEmbInformeEmbarazo))
+          .catch(error => res.status(400).send(error));
+      },
+    
+      update(req, res) {
+        console.log(UeggEmbInformeEmbarazo);
+        return UeggEmbInformeEmbarazo.findByPk(req.params.Id, {})
+          .then(ueggEmbInformeEmbarazo => {
+            if (!ueggEmbInformeEmbarazo) {
+              return res.status(404).send({
+                message: "ueggEmbInformeEmbarazo Not Found"
+              });
+            }
+            return ueggEmbInformeEmbarazo
+              .update({
+                id_pcpa_unidad_eductiva: req.body.id_pcpa_unidad_eductiva || ueggEmbInformeEmbarazo.id_pcpa_unidad_eductiva,
+                id_emb_reporte_embarazo_tipo_1: req.body.id_emb_reporte_embarazo_tipo_1 || ueggEmbInformeEmbarazo.id_emb_reporte_embarazo_tipo_1, 
+                id_emb_reporte_embarazo_tipo_2: req.body.id_emb_reporte_embarazo_tipo_2 || ueggEmbInformeEmbarazo.id_emb_reporte_embarazo_tipo_2, 
+                id_emb_reporte_embarazo_tipo_3: req.body.id_emb_reporte_embarazo_tipo_3 || ueggEmbInformeEmbarazo.id_emb_reporte_embarazo_tipo_3, 
+
+                estado: 'MODIFICADO',  
+                usu_mod: req.body.usu_mod ,
+                fec_mod: req.body.fec_mod
+              })
+              .then(() =>{  
+                 console.log(' *************SI UPDATE OK');
+                 res.status(200).send(ueggEmbInformeEmbarazo)   })
+              .catch(error => {
+                console.log(' *************ERROR UPDATE 1', error);
+                res.status(400).send(error)  });
+          })
+          .catch(error => {
+            console.log(' *************ERROR UPDATE 2',  error);
+            res.status(400).send(error)  });
+      },
+    
+      delete(req, res) {
+        return UeggEmbInformeEmbarazo.findByPk(req.params.Id)
+          .then(ueggEmbInformeEmbarazo => {
+            if (!ueggEmbInformeEmbarazo) {
+              return res.status(400).send({
+                message: "ueggEmbInformeEmbarazo Not Found"
+              });
+            }
+            return ueggEmbInformeEmbarazo
+              .destroy()
+              .then(() =>{
+                console.log(' ************SI DELETE OK');
+                 res.status(204).send() }  )
+              .catch(error => res.status(400).send(error));
+          })
+          .catch(error => res.status(400).send(error));
+      }
+
 
 };
--- models/uegg/uegg_emb_estudiante_derechos.js
+++ models/uegg/uegg_emb_estudiante_derechos.js
@@ -17,41 +17,41 @@
         autoIncrement: true,
         field: 'id'
     },
-	id_pcpa_unidad_eductiva: DataTypes.INTEGER,
-    id_emb_informe_embarazo: DataTypes.INTEGER,	
-	
-    cod_rude : DataTypes.STRING,	
-    nombres_apellidos: DataTypes.STRING,	
-	
-    complemento: DataTypes.STRING,		
-    fec_nacimiento: DataTypes.DATE,	
-    tiempo_gestacion: DataTypes.INTEGER,
-    numero_embarazos: DataTypes.INTEGER,
-    fec_estimada_parto: DataTypes.DATE,	
-    check_recibe_control_prenatal: DataTypes.BOOLEAN,	
-check_requiere_baja_medica: DataTypes.BOOLEAN,	
-check_requiere_permiso_para_control: DataTypes.BOOLEAN,	
-check_requiere_cuidado_especial: DataTypes.BOOLEAN,	
-check_cuenta_con_medidas_ue: DataTypes.BOOLEAN,	
-   // fec_baja_prenatal: DataTypes.DATE,	
-     //   fec_baja_postnatal detalle,	
-            fec_retorno_a_ue: DataTypes.DATE,	
-	
-check_tutores_al_tanto_emb: DataTypes.BOOLEAN,	
-check_recibe_baja_medica: DataTypes.BOOLEAN,	
-check_recibe_permiso_controles_prenatales: DataTypes.BOOLEAN,	
-            fec_ini_baja_prenatal: DataTypes.DATE,	
-            fec_fin_baja_postnatal: DataTypes.DATE,	
-	
-	persona_asignada_seguimiento : DataTypes.STRING,
-    check_cuenta_con_seguimiento: DataTypes.BOOLEAN,	
- 	
-    estado: DataTypes.STRING,	
-    usu_cre: DataTypes.STRING,	
-    usu_mod: DataTypes.STRING,	
-    fec_cre: DataTypes.DATE,	
-    fec_mod: DataTypes.DATE	
-	
+        id_pcpa_unidad_eductiva: DataTypes.INTEGER,
+        id_emb_informe_embarazo: DataTypes.INTEGER,	
+        
+        cod_rude : DataTypes.STRING,	
+        nombres_apellidos: DataTypes.STRING,	
+        
+        complemento: DataTypes.STRING,		
+        fec_nacimiento: DataTypes.DATE,	
+        tiempo_gestacion: DataTypes.INTEGER,
+        numero_embarazos: DataTypes.INTEGER,
+        fec_estimada_parto: DataTypes.DATE,	
+        check_recibe_control_prenatal: DataTypes.BOOLEAN,	
+        check_requiere_baja_medica: DataTypes.BOOLEAN,	
+        check_requiere_permiso_para_control: DataTypes.BOOLEAN,	
+        check_requiere_cuidado_especial: DataTypes.BOOLEAN,	
+        check_cuenta_con_medidas_ue: DataTypes.BOOLEAN,	
+        // fec_baja_prenatal: DataTypes.DATE,	
+        //   fec_baja_postnatal detalle,	
+        fec_retorno_a_ue: DataTypes.DATE,	
+        
+        check_tutores_al_tanto_emb: DataTypes.BOOLEAN,	
+        check_recibe_baja_medica: DataTypes.BOOLEAN,	
+        check_recibe_permiso_controles_prenatales: DataTypes.BOOLEAN,	
+        fec_ini_baja_prenatal: DataTypes.DATE,	
+        fec_fin_baja_postnatal: DataTypes.DATE,	
+        
+        persona_asignada_seguimiento : DataTypes.STRING,
+        check_cuenta_con_seguimiento: DataTypes.BOOLEAN,	
+        
+        estado: DataTypes.STRING,	
+        usu_cre: DataTypes.STRING,	
+        usu_mod: DataTypes.STRING,	
+        fec_cre: DataTypes.DATE,	
+        fec_mod: DataTypes.DATE	
+        
   }, {         	
   tableName: 'uegg_emb_estudiante_derechos',      	
   timestamps: false,     });   	
